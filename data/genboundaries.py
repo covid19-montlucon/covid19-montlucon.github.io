0fe1056a7fe3dfb0fb81cf12349511d4de695f50
Kaylier <st.heudron@gmail.com>
2020-04-03 22:06:47 +0200

    Change categories

--- data/genboundaries.py
+++ data/genboundaries.py
@@ -11,7 +11,11 @@ locs = {cas[2] for cas in cases}
 for loc in locs:
     print(loc)
 
-newdata = {'type': 'FeatureCollection', 'features': [feat for feat in data['features'] if feat['properties']['name'] in locs]}
+ambigus = {'Saint-Hilaire', 'Saint-Loup', 'Chappes', 'La Celle', 'Lussat', 'Saint-Angel'}
+ambigu_insee = {"03238", "03242", "03058", "03047", "23114", "03217"}
+
+newdata = {'type': 'FeatureCollection', 'features': [feat for feat in data['features'] if feat['properties']['name'] in locs-ambigus or feat['properties']['name'] in locs and feat['properties']['insee'] in ambigu_insee]}
+
 
 with open('boundaries.GeoJson', 'w') as f:
     json.dump(newdata, f)
