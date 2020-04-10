e569470dbd8b5647fde1b7c33edef4258af319ac
Kaylier <st.heudron@gmail.com>
2020-04-10 17:07:28 +0200

    Add merging of communes, when the name is CommuneA/CommuneB

--- data/genboundaries.py
+++ data/genboundaries.py
@@ -8,14 +8,63 @@ with open('listCases.csv', 'r') as f:
     cases = [l.split(',') for l in f]
 
 locs = {cas[2] for cas in cases}
+merges = []
 for loc in locs:
-    print(loc)
+    if loc == '#N/A':
+        continue
+    if '/' in loc:
+        merges.append(loc.split('/'))
+if any(loc in ms for ms in merges for loc in locs):
+    print('Warning: isolated location')
+if any(set(m1)&set(m2) for m1 in merges for m2 in merges if m1 != m2):
+    print('Warning: overlapping regions')
 
 ambigus = {'Saint-Hilaire', 'Saint-Loup', 'Chappes', 'La Celle', 'Lussat', 'Saint-Angel'}
 ambigu_insee = {"03238", "03242", "03058", "03047", "23114", "03217"}
 
-newdata = {'type': 'FeatureCollection', 'features': [feat for feat in data['features'] if feat['properties']['name'] in locs-ambigus or feat['properties']['name'] in locs and feat['properties']['insee'] in ambigu_insee]}
+newdata = {'type': 'FeatureCollection', 'features': []}
 
+waiting_merge = {}
+for feat in data['features']:
+    for ms in merges:
+        if feat['properties']['name'] in ms:
+            waiting_merge[feat['properties']['name']] = feat
+            break
+    else:
+        if feat['properties']['name'] not in locs:
+            continue
+        if feat['properties']['name'] in ambigus and feat['properties']['insee'] not in ambigu_insee:
+            continue
+        if feat['properties']['population'] < 400:
+            print('Warning: large population for ' + feat['properties']['name'])
+        newdata['features'].append(feat)
+
+for ms in merges:
+    ms = [name for name in ms if name in waiting_merge]
+    feats = [waiting_merge[name] for name in ms]
+    newdata['features'].append({
+        'type': 'Feature',
+        'properties': {
+            'name': '/'.join(sorted(ms)),
+            'population': sum(feat['properties']['population'] for feat in feats),
+            'coordinates': {
+                'latitude': sum(feat['properties']['coordinates']['latitude'] for feat in feats)/len(ms),
+                'longitude': sum(feat['properties']['coordinates']['longitude'] for feat in feats)/len(ms),
+                }
+            },
+        'bbox': [
+            min(feat['bbox'][0] for feat in feats),
+            min(feat['bbox'][1] for feat in feats),
+            max(feat['bbox'][2] for feat in feats),
+            max(feat['bbox'][3] for feat in feats),
+            ],
+        'geometry': {
+            'type': 'MultiPolygon',
+            'coordinates': [
+                    feat['geometry']['coordinates'][0] for feat in feats
+                ]
+            }
+        })
 
 with open('boundaries.GeoJson', 'w') as f:
     json.dump(newdata, f)
